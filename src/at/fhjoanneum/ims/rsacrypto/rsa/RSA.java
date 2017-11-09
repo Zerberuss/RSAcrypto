aa890f3d149efc5fae979f46e2607e8872fbba17
Timo Hasenbichler <t.hasenbichler@netconomy.net>
2017-11-09 12:50:02 +0100

    added decryption key generation

--- src/at/fhjoanneum/ims/rsacrypto/rsa/RSA.java
+++ src/at/fhjoanneum/ims/rsacrypto/rsa/RSA.java
@@ -2,7 +2,9 @@ package at.fhjoanneum.ims.rsacrypto.rsa;
 
 import at.fhjoanneum.ims.rsacrypto.imsCrypto.ImsInteger;
 
+
 public class RSA {
+
     private ImsInteger p;
     private ImsInteger q;
     private ImsInteger e;
@@ -17,13 +19,44 @@ public class RSA {
     }
 
     public RSA(ImsInteger n, boolean optimized) {
+
+        //generate primes    n > p*q;
+
         this.n = n;
+        if (optimized) {
+            //genreate pq
+            //hardcoded e  2^16+1
+            this.e = modPow(ImsInteger.valueOf(16), ImsInteger.ONE, ImsInteger.valueOf(2)).add(ImsInteger.ONE);
+            this.d = generateDecryptionKey();
+        } else {
+            //generate pq
+            //generate e
+            this.d = generateDecryptionKey();
+
+        }
         this.optimized = optimized;
     }
 
     private ImsInteger getP() {
         return p;
     }
+
+    private ImsInteger generateDecryptionKey() {
+        return (ImsInteger.ONE.divide(this.e)).getPhi();
+    }
+
+    private ImsInteger modPow(ImsInteger exponent, ImsInteger mod, ImsInteger base) {
+        ImsInteger result = ImsInteger.ONE;
+        for (int i = 0; i < exponent.bitLength(); i++) {
+            if (exponent.testBit(i)) {
+                result = (result.multiply(base)).mod(mod);
+            }
+            base = (base.multiply(base)).mod(mod);
+        }
+        return result.mod(mod);
+
+    }
+
 
     private ImsInteger getQ() {
         return q;
