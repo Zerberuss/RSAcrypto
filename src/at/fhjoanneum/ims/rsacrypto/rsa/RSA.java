ecff664d406db811e915a744f457d12165fe770b
timoooo <timo.hasenbichler@protonmail.com>
2017-11-09 14:08:00 +0100

    Merge branch 'develop' of https://github.com/Zerberuss/RSAcrypto into develop
    # Conflicts:
    #	src/at/fhjoanneum/ims/rsacrypto/rsa/RSA.java

--- src/at/fhjoanneum/ims/rsacrypto/rsa/RSA.java
+++ src/at/fhjoanneum/ims/rsacrypto/rsa/RSA.java
@@ -1,17 +1,22 @@
 package at.fhjoanneum.ims.rsacrypto.rsa;
 
 import at.fhjoanneum.ims.rsacrypto.imsCrypto.ImsInteger;
+import com.oracle.tools.packager.Log;
+
+import java.util.Random;
 import sun.awt.InputMethodSupport;
 
 import java.math.BigInteger;
 
-
 public class RSA {
+    private static int numOfPrimeTests = 40; // how often the primality test has to be passed in order to be accepted as a prime
+    private static int informEveryNthNumber = 10000; // after that many trials to find a at.fhjoanneum.ims.rsacrypto.carmichael number, the user is informed
+
 
     private ImsInteger p;
     private ImsInteger q;
     private ImsInteger e;
-    private ImsInteger n;
+    private int n;
     private ImsInteger d;
     private boolean optimized;
 
@@ -73,8 +78,37 @@ public class RSA {
         return e;
     }
 
-    public ImsInteger getN() {
+    public int getN() {
         return n;
+    }
+
+    public void generatePQ() {
+        //generate random numbers
+        ImsInteger m = new ImsInteger(this.n/2, new Random());
+        ImsInteger counter = ImsInteger.ZERO; // used to determine when to output information
+
+
+        // increase m by one until all terms are prime
+        do{
+            counter=counter.add(ImsInteger.ONE);
+            m = m.add(ImsInteger.ONE);
+
+            // inform the user how many numbers we already checked
+            if(counter.mod(ImsInteger.valueOf(informEveryNthNumber)).compareTo(ImsInteger.ZERO)==0)
+            {
+                System.out.println("Checked " + counter + " numbers, current basis: " + m);
+            }
+
+            p = ImsInteger.valueOf(1).multiply(m).add(ImsInteger.ONE);
+            q = ImsInteger.valueOf(2).multiply(m).add(ImsInteger.ONE);
+
+
+        }while(
+                p.isProbablePrime(numOfPrimeTests) && q.isProbablePrime(numOfPrimeTests)
+        );
+
+        Log.info("Generated p: " +  p.toString() + ", q: " + q.toString());
+        System.out.println("Generated p: " +  p.toString() + ", q: " + q.toString() + ", m: " + m.toString());
     }
 
     public ImsInteger encrypt(ImsInteger x, RSAKey pubKey) {
