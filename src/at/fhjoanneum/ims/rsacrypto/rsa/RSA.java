d4f9c2d30896ceec7bc079b8a6fb40d769cb84a1
Maximilian Zollneritsch <zollneritsch@gmail.com>
2017-11-09 11:40:26 +0100

    added second constructor

--- src/at/fhjoanneum/ims/rsacrypto/rsa/RSA.java
+++ src/at/fhjoanneum/ims/rsacrypto/rsa/RSA.java
@@ -8,11 +8,17 @@ public class RSA {
     private ImsInteger e;
     private ImsInteger n;
     private ImsInteger d;
+    private boolean optimized;
 
     public RSA(ImsInteger p, ImsInteger q, ImsInteger e) {
         this.p = p;
         this.q = q;
         this.e = q;
+    }
+
+    public RSA(ImsInteger n, boolean optimized) {
+        this.n = n;
+        this.optimized = optimized;
     }
 
     private ImsInteger getP() {
